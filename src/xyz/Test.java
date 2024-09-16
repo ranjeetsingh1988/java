336bd4d536a9f45b85a0b7f935c29a483ca75451
nikhil.jain <nikhil.jain@techrbm.com>
2024-09-16 18:09:34 +0530

    Update

---
+++ src/xyz/Test.java
@@ -0,0 +1,24 @@
+package xyz;
+
+import java.util.Scanner;
+public class Test {
+
+	public static void main(String[] args) {
+	int num = 0, a = 1;
+	System.out.println("Please enter student marks");
+	Scanner sc = new Scanner(System.in);
+	num = sc.nextInt();
+	
+	if(num >= 90) {
+		System.out.println("You got A+ Marks");
+	}else if(num >= 75) {
+		System.out.println("You Got A Marks");
+	}else if(num >= 50 ) {
+		System.out.println("You Got B Marks");
+	}else
+		System.out.println("You Got C Marks");
+	
+
+	}
+
+}
