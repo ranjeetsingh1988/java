3858dc0f0e1127b72c6fa50d2137eec8815b2e2b
nikhil.jain <nikhil.jain@techrbm.com>
2024-09-24 14:05:53 +0530

    update

---
+++ src/xyz/doWhileLoop.java
@@ -0,0 +1,24 @@
+package xyz;
+
+public class doWhileLoop {
+
+	public static void main(String[] args) {
+		int a = 10;
+		do {
+			System.out.println(a);
+			a--;
+			
+		}while(a>=5);
+		
+		int b = 100;
+		do {
+			System.out.println(b);
+			b++;
+			
+		}while(b<=200);
+
+
+	}
+
+}
+
